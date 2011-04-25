bee76934dd2f6c660fa3516a6cef016ca7937188
Brian Patt <blpatt@mit.edu>
2011-04-25 12:58:48 -0400

    fix some compiler warnings

--- src/edu/mit/mitmobile2/events/EventsModel.java
+++ src/edu/mit/mitmobile2/events/EventsModel.java
@@ -1,10 +1,8 @@
 package edu.mit.mitmobile2.events;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
