040ae037334516d9a2abd75ec5f190b4bad4ede6
Brian Patt <blpatt@mit.edu>
2011-06-30 16:50:05 -0400

    use TwoLineActionRow's for the list of categories

--- src/edu/mit/mitmobile2/facilities/CategoryAdapter.java
+++ src/edu/mit/mitmobile2/facilities/CategoryAdapter.java
@@ -2,23 +2,16 @@ package edu.mit.mitmobile2.facilities;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import edu.mit.mitmobile2.R;
+import edu.mit.mitmobile2.TwoLineActionRow;
 import edu.mit.mitmobile2.objs.FacilitiesItem.CategoryRecord;
 
 class CategoryAdapter extends CursorAdapter {
-
-	private Context mContext;
 	
 	public CategoryAdapter(Context context, Cursor cursor) {
 		super(context, cursor);
-		
-		mContext = context;
 	}
 	
 	private void setupRow(Cursor cursor, View row) {
@@ -26,25 +19,19 @@ class CategoryAdapter extends CursorAdapter {
 		categoryRecord.id = cursor.getString(1);
 		categoryRecord.name = cursor.getString(2);
 		
-		TextView categoryTV = (TextView) row.findViewById(R.id.facilitiesRowTV);
-		
-		categoryTV.setText(categoryRecord.name,  TextView.BufferType.SPANNABLE);
-		
-		
-		int separator = categoryRecord.name.length() + 1;
+		TwoLineActionRow actionRow = (TwoLineActionRow) row;
+		actionRow.setTitle(categoryRecord.name);		
 		
 	}
 	
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
-		LinearLayout row = (LinearLayout) view;
-		setupRow(cursor, row);
+		setupRow(cursor, view);
 	}
 
 	@Override
 	public View newView(Context context, Cursor cursor, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		LinearLayout row = (LinearLayout) inflater.inflate(R.layout.facilities_row, null);
+		TwoLineActionRow row = new TwoLineActionRow(context);
 		setupRow(cursor, row);
 		return row;
 	}
