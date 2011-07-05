fa4130558eda3dd6e653e6aa5fab0dffae8ea369
Brian Patt <blpatt@mit.edu>
2011-07-05 17:57:39 -0400

    fix the title of the problem type location fix a couple compiler warnings

--- src/edu/mit/mitmobile2/facilities/LocationAdapter.java
+++ src/edu/mit/mitmobile2/facilities/LocationAdapter.java
@@ -1,13 +1,10 @@
 package edu.mit.mitmobile2.facilities;
 
 import edu.mit.mitmobile2.R;
-import edu.mit.mitmobile2.facilities.FacilitiesDB.LocationCategoryTable;
-import edu.mit.mitmobile2.facilities.FacilitiesDB.LocationTable;
 import edu.mit.mitmobile2.objs.FacilitiesItem.LocationRecord;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.text.style.TextAppearanceSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
