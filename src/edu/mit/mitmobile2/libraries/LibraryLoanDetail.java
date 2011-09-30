0a0fbc6218b50ea4bf01615b105203146c217a41
Seth Seligman <sseligma@mit.edu>
2011-09-30 17:08:04 -0400

    added fields to library load detail and cleaned up formatting

--- src/edu/mit/mitmobile2/libraries/LibraryLoanDetail.java
+++ src/edu/mit/mitmobile2/libraries/LibraryLoanDetail.java
@@ -1,31 +1,24 @@
 package edu.mit.mitmobile2.libraries;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.WeakHashMap;
-
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.view.Menu;
+import android.text.Html;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-import edu.mit.mitmobile2.Module;
 import edu.mit.mitmobile2.R;
-import edu.mit.mitmobile2.SliderActivity;
 import edu.mit.mitmobile2.objs.LoanListItem;
 
 public class LibraryLoanDetail extends Activity{
-    private TextView loanTitleTV;
+	public static final String TAG = "LibraryLoanDetail";
+
+	private TextView loanTitleTV;
     private TextView loanAuthorTV;
     private TextView loanCallNoTV;
 	private TextView loanLibraryTV;
 	private TextView loanISBNTV;
 	private TextView loanOverdueTV;
 	private Button loanRenewButton;
-    
     private int index;
     
 //        Intent intent = new Intent(context, LibraryDetailActivity.class);
@@ -41,24 +34,24 @@ public class LibraryLoanDetail extends Activity{
         Bundle extras = getIntent().getExtras();
         index = extras.getInt("index");
         LoanListItem item = LibraryLoans.getLoanData().getLoans().get(index);
-
+        
         loanTitleTV = (TextView)findViewById(R.id.loanTitleTV);
         loanTitleTV.setText(item.getTitle());
 
         loanAuthorTV = (TextView)findViewById(R.id.loanAuthorTV);
-        loanAuthorTV.setText(item.getYear() + " " + item.getAuthor());
+        loanAuthorTV.setText(item.getYear() + "; " + item.getAuthor());
 
         loanCallNoTV = (TextView)findViewById(R.id.loanCallNoTV);
         loanCallNoTV.setText(item.getCallNo());
       
         loanLibraryTV = (TextView)findViewById(R.id.loanLibraryTV);
-        loanLibraryTV.setText("");
+        loanLibraryTV.setText(item.getSubLibrary());
 
         loanISBNTV = (TextView)findViewById(R.id.loanISBNTV);
         loanISBNTV.setText(item.getIsbnIssnDisplay());
-
+       
         loanOverdueTV = (TextView)findViewById(R.id.loanOverdueTV);
-        loanOverdueTV.setText(item.getDueText());
+        loanOverdueTV.setText(Html.fromHtml(item.getDueText()));
 
     }
     
