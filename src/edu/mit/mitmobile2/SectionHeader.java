90511ceb98d1602e67113c7f33d7d079cdc43cbf
Brian Patt <blpatt@mit.edu>
2011-12-05 17:44:13 -0500

    add a constructor for section header, that allows to specifify if primary versus secondary.

--- src/edu/mit/mitmobile2/SectionHeader.java
+++ src/edu/mit/mitmobile2/SectionHeader.java
@@ -14,27 +14,35 @@ public class SectionHeader extends FrameLayout {
 	private TextView mTextView;
 	private ImageView mBackgroundView;
 	
-	private static int PRIMARY = 0;
-	private static int SECONDARY = 1;
+	public enum Prominence {
+		PRIMARY,
+		SECONDARY
+	}
 	
 	public SectionHeader(Context context, AttributeSet attributeSet) {
 			super(context, attributeSet);
 			
 			String initialText = attributeSet.getAttributeValue("http://schemas.android.com/apk/res/android", "text");
-			int prominence = attributeSet.getAttributeIntValue("http://schemas.android.com/apk/res/" + BuildSettings.release_project_name, "prominence", PRIMARY);
+			int prominenceInt = attributeSet.getAttributeIntValue("http://schemas.android.com/apk/res/" + BuildSettings.release_project_name, "prominence", 0);
+			Prominence prominence = Prominence.values()[prominenceInt];
 			initializeHelper(context, initialText, prominence);
 	}
 	
 	public SectionHeader(Context context, String initialText) {
 		super(context);
-		initializeHelper(context, initialText, PRIMARY);
+		initializeHelper(context, initialText, Prominence.PRIMARY);
+	}
+	
+	public SectionHeader(Context context, String initialText, Prominence prominence) {
+		super(context);
+		initializeHelper(context, initialText, prominence);
 	}
 	
 	public void setBackgroundResourceId(int resourceId) {
 		mBackgroundView.setImageResource(resourceId);
 	}
 	
-	private void initializeHelper(Context context, String initialText, int prominence) {
+	private void initializeHelper(Context context, String initialText, Prominence prominence) {
 		LayoutInflater inflator = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		inflator.inflate(R.layout.section_header, this);
 		
@@ -43,9 +51,9 @@ public class SectionHeader extends FrameLayout {
 		
 		mBackgroundView = (ImageView) findViewById(R.id.sectionHeaderBackgroundIV);
 		
-		if(prominence == PRIMARY) {
+		if(prominence == Prominence.PRIMARY) {
 			setBackgroundResourceId(R.drawable.list_subhead);
-		} else if(prominence == SECONDARY) {
+		} else if(prominence == Prominence.SECONDARY) {
 			setBackgroundResourceId(R.drawable.list_subhead_gray);
 		}
 		
@@ -58,5 +66,6 @@ public class SectionHeader extends FrameLayout {
 	public void setText(String text) {
 		mTextView.setText(text);
 	}
+	
 }
 
