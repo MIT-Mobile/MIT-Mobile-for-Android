MIT-Mobile/MIT-Mobile-for-Android

commit 6d7303055a4baba20c44d6a885b936fe3ea112bb

improve look of thank you screen for tell us form (go the the librarie home activity) - add a more thorough constructor for the ActionRow

src/edu/mit/mitmobile2/ActionRow.java
@@ -1,6 +1,7 @@
 package edu.mit.mitmobile2;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -22,7 +23,8 @@ public abstract class ActionRow extends FrameLayout {
 	}
 	
 	private Drawable mSystemSelectionDrawable;
-	private int mBackgroundResourceId;
+	private int mBackgroundResourceId = -1;
+	private Integer mBackgroundColor;
 	
 	
 	public ActionRow(Context context, AttributeSet attrs) {
@@ -32,7 +34,12 @@ public abstract class ActionRow extends FrameLayout {
 
 		mBackgroundResourceId = attrs.getAttributeResourceValue("http://schemas.android.com/apk/res/android", "background", -1);
 		if(mBackgroundResourceId == -1) {
-			mBackgroundResourceId = R.color.rowBackground;
+			String colorString = attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "background");
+			if(colorString != null) {
+				mBackgroundColor = Color.parseColor(colorString);
+			} else {
+				mBackgroundResourceId = R.color.rowBackground;
+			}
 		}
 	}
 
@@ -66,7 +73,11 @@ public abstract class ActionRow extends FrameLayout {
 		}
 		
 		if(event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) {
-			setBackgroundResource(mBackgroundResourceId);
+			if (mBackgroundColor != null) {
+				setBackgroundColor(mBackgroundColor);
+			} else {
+				setBackgroundResource(mBackgroundResourceId);
+			}
 			setPressed(false);
 		}
 		

res/layout/library_form_thank_you.xml
@@ -4,8 +4,7 @@
     android:id="@+id/librariesFormThankYou"
     android:layout_width="fill_parent"
     android:layout_height="fill_parent"
-    android:fillViewport="true"
-    android:background="@color/rowBackground"
+    
     >
     
     <LinearLayout
@@ -17,6 +16,7 @@
     	<edu.mit.mitmobile2.TwoLineActionRow
     		android:layout_width="fill_parent"
     		android:layout_height="wrap_content"
+    		android:background="@color/rowBackground"
     		android:id="@+id/librariesThankYouContentActionRow"
     		/>
     	
@@ -26,10 +26,18 @@
     		android:background="@color/dividerColor"
     		/>
     	
+    	<View
+    		android:layout_width="fill_parent"
+    		android:layout_height="30dip"
+    		android:background="@drawable/main_repeat"
+    		/>
+    		
     	<edu.mit.mitmobile2.TwoLineActionRow
-    		android:text="Return home"
+    		android:text="Return Libaries home"
     		android:layout_width="fill_parent"
     		android:layout_height="wrap_content"
+    		android:background="#990000"
+    		android:textColor="#FFFFFF"
     		android:id="@+id/librariesThankYouReturnHome"
     		/>	    	
     		

src/edu/mit/mitmobile2/SearchBar.java
@@ -16,6 +16,8 @@ public class SearchBar extends LinearLayout {
 	ImageButton mSearchButton;
 	TextView mSearchHintTV;
 	
+	private static final String NAMESPACE = "http://schemas.android.com/apk/res/android";
+
 	public SearchBar(Context context, AttributeSet attributeSet) {
 		super(context, attributeSet);
 		
@@ -26,6 +28,11 @@ public class SearchBar extends LinearLayout {
 		mSearchBox.setFocusableInTouchMode(true);
 		mSearchButton = (ImageButton) findViewById(R.id.searchButton);
 		mSearchHintTV = (TextView) findViewById(R.id.searchBoxHint);
+		
+		if(attributeSet != null) {
+			String hintText = attributeSet.getAttributeValue(NAMESPACE, "hint");
+			setSearchHint(hintText);
+		}
 	}
 	
 	public static interface OnInitiateSearchListener {

src/edu/mit/mitmobile2/libraries/TellUsActivity.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -65,9 +66,11 @@ public class TellUsActivity extends ModuleActivity {
         mGoHomeButton = (TwoLineActionRow) findViewById(R.id.librariesThankYouReturnHome);
         mGoHomeButton.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v) {
-				MITNewsWidgetActivity.goHome(mContext);
-			}
+		    public void onClick(View v) {
+				Intent intent = new Intent(mContext, getModule().getModuleHomeActivity());
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+		    }
 		});
         
         statusArray = getResources().getStringArray(R.array.libraryStatus);

commit 5bed684890bb8f5c72f0bcf55898da5bf88171fa

fix bug introduced by a previous commit (6d7303055a4baba20c44d6a885b936fe3ea112bb)

src/edu/mit/mitmobile2/ActionRow.java
@@ -46,6 +46,7 @@ public abstract class ActionRow extends FrameLayout {
 	public ActionRow(Context context) {
 		super(context);
 		initHelper(context);
+		mBackgroundResourceId = R.color.rowBackground;
 	}
 	
 	private void initHelper(Context context) {