d296dd450acdce091c2a7a3befcd3fec90db52aa
Austin Emmons <austin.emmons@modolabs.com>
2012-10-31 18:27:18 -0400

    [ANDROIDAPP-567]
    Added Ant Builder 'ant-config.xml' that copies the template config/Config.java file and replaces various keyed values with their most recent values.
    Current Values:
    	BUILD_GIT_DESCRIBE
    	BUILD_ID
    
    These values can then be accessed in code by calling the Config class.

---
+++ config/Config.java
@@ -0,0 +1,7 @@
+package edu.mit.mitmobile2.about;
+
+public class Config
+{
+	public final static String BUILD_GIT_DESCRIBE = "@GIT.DESCRIBE@";	
+	public final static String BUILD_ID = "@GIT.HASH@";
+}
