6b3ed606c4f5a417199dab7cc47822e8844ea04b
Nelson Chen <njchen@mit.edu>
2012-11-28 16:16:53 -0500

    ANDROIDAPP-570, remove info button in actionbar in facilities

--- src/edu/mit/mitmobile2/facilities/FacilitiesModule.java
+++ src/edu/mit/mitmobile2/facilities/FacilitiesModule.java
@@ -1,10 +1,8 @@
 package edu.mit.mitmobile2.facilities;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Intent;
 import edu.mit.mitmobile2.MITMenuItem;
 import edu.mit.mitmobile2.NewModule;
 import edu.mit.mitmobile2.R;
@@ -44,9 +42,8 @@ public class FacilitiesModule extends NewModule {
 	@Override
 	public List<MITMenuItem> getPrimaryOptions() {
 		// TODO Auto-generated method stub
-		List<MITMenuItem> menuItems = new ArrayList<MITMenuItem>();
-		menuItems.add(new MITMenuItem("menu_info", "", R.drawable.menu_info));
-		return menuItems;
+
+		return null;
 	}
 
 	@Override
@@ -58,10 +55,7 @@ public class FacilitiesModule extends NewModule {
 	@Override
 	public boolean onItemSelected(Activity activity, String id) {
 		// TODO Auto-generated method stub
-		if (id.equals("menu_info")) {
-			Intent intent = new Intent(activity, FacilitiesInfoActivity.class);					
-			activity.startActivity(intent);
-		}
+
 		return false;
 	}
 }
