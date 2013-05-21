fdb24a0099fcfc65f811e532ee9bdec7e62de245
Brian Patt <blpatt@mit.edu>
2013-05-21 11:24:23 -0400

    show meal name, and meal date in comparison view.

--- src/edu/mit/mitmobile2/dining/LandscapeDiningScheduleScreen.java
+++ src/edu/mit/mitmobile2/dining/LandscapeDiningScheduleScreen.java
@@ -5,12 +5,16 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 import edu.mit.mitmobile2.R;
+import edu.mit.mitmobile2.SliderView;
+import edu.mit.mitmobile2.SliderView.Adapter;
+import edu.mit.mitmobile2.SliderView.OnSeekListener;
 import edu.mit.mitmobile2.dining.DiningModel.DiningVenues;
 import edu.mit.mitmobile2.dining.DiningModel.HouseDiningHall;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 public class LandscapeDiningScheduleScreen extends DiningScheduleScreen {
 
@@ -27,9 +31,24 @@ public class LandscapeDiningScheduleScreen extends DiningScheduleScreen {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View view = inflater.inflate(R.layout.dining_comparison, null);
 		DiningSliderView sliderView = (DiningSliderView) view.findViewById(R.id.diningComparisonSliderView);
+		final TextView dateView = (TextView) view.findViewById(R.id.diningComparisonDate);
+		
 		@SuppressWarnings("unchecked")
 		List<HouseDiningHall> halls =  (List<HouseDiningHall>) mVenues.getHouses();
-		sliderView.setAdapter(new DiningComparisionSliderAdapter(context, halls, mInitialDate.getTimeInMillis()));
+		
+		DiningComparisionSliderAdapter diningAdapter = new DiningComparisionSliderAdapter(context, halls, mInitialDate.getTimeInMillis());
+		dateView.setText(diningAdapter.getCurrentTitle());	
+		
+		sliderView.setAdapter(diningAdapter);		
+		sliderView.setOnSeekListener(new OnSeekListener() {
+
+			@Override
+			public void onSeek(SliderView view, Adapter adapter) {
+				DiningComparisionSliderAdapter diningAdapter = (DiningComparisionSliderAdapter) adapter;
+				dateView.setText(diningAdapter.getCurrentTitle());
+			}			
+		});
+		
 		return view;
 	}
 
