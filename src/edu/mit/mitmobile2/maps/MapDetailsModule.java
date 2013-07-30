af5e6e33228f4f06f50458278ee2593997dec418
Seth Seligman <sseligma@mit.edu>
2013-07-30 15:50:37 -0400

    removed menu options from Map Details screen

---
+++ src/edu/mit/mitmobile2/maps/MapDetailsModule.java
@@ -0,0 +1,29 @@
+package edu.mit.mitmobile2.maps;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import edu.mit.mitmobile2.CommonActions;
+import edu.mit.mitmobile2.MITMenuItem;
+import edu.mit.mitmobile2.NewModule;
+import edu.mit.mitmobile2.R;
+
+public class MapDetailsModule extends MapsModule {
+
+	@Override
+	public List<MITMenuItem> getPrimaryOptions() {
+		return Arrays.asList();
+	}
+
+	@Override
+	public List<MITMenuItem> getSecondaryOptions() {
+		return Arrays.asList();
+	}
+
+		
+}
