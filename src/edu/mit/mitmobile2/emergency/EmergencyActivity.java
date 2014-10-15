e6d840752bf15f6d778fb70363777201cc9ef280
sseligma <sseligma@mit.edu>
2014-10-15 15:52:20 -0400

    Fixed issue 29 where only the first emergency contact was displayed.

--- src/edu/mit/mitmobile2/emergency/EmergencyActivity.java
+++ src/edu/mit/mitmobile2/emergency/EmergencyActivity.java
@@ -2,11 +2,9 @@ package edu.mit.mitmobile2.emergency;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshAttacher;
 import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshAttacher.OnRefreshListener;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -22,12 +20,9 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.TextView;
 import android.widget.Toast;
+import edu.mit.mitmobile2.FullScreenLoader;
 import edu.mit.mitmobile2.Global;
-import edu.mit.mitmobile2.LoadingUIHelper;
-import edu.mit.mitmobile2.MITMenuItem;
 import edu.mit.mitmobile2.NewModule;
 import edu.mit.mitmobile2.NewModuleActivity;
 import edu.mit.mitmobile2.R;
@@ -38,49 +33,106 @@ import edu.mit.mitmobile2.objs.EmergencyItem.Contact;
 
 
 public class EmergencyActivity extends NewModuleActivity implements OnRefreshListener {
-
-	private ListView mListView;
-
-	private Context mContext;
-	private WebView mEmergencyMsgTV = null;
-	private ImageView mEmergencyMsgLoader;
-	private ImageView mEmergencyListLoader;
-	private ScrollView mEmergencyScrollView;
 	
-	TextView emergencyContactsTV;
-
-	SharedPreferences pref;
-
-	private PullToRefreshAttacher mPullToRefreshAttacher;
-
-	
+	FullScreenLoader mLoadingView;
 	static EmergencyItem emergencyItem;
+	private WebView mEmergencyMsgTV = null;
+	SharedPreferences pref;
+	private Context mContext;
+	private PullToRefreshAttacher msgPullToRefreshAttacher;
+	private PullToRefreshAttacher contactsPullToRefreshAttacher;
+	
+	private ListView contactsListView;
+	private ImageView emergencyMsgLoader;
+	private ImageView emergencyListLoader;
 	
 	static String PREF_KEY_EMERGENCY_TEXT = "emergency_text";
+	private static String noticeTemplate = "<html><body style=\"padding: 7px;\">%s\n<p>Posted %s</p></body></html>";
+	private Handler uiContactsHandler;
+	private Handler uiStatusHandler;
 	
-	/****************************************************/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		mContext = this;
+		setContentView(R.layout.emergency);		
+		setTitle("Emergeny Contacts");
 		
-		setContentView(R.layout.emergency);
-		
-		mEmergencyMsgLoader = (ImageView) findViewById(R.id.emergencyMsgLoader);
-		mEmergencyListLoader = (ImageView) findViewById(R.id.emergencyListLoader);
-		mEmergencyScrollView = (ScrollView) findViewById(R.id.emergencyMainScrollView);
-		
-		pref = this.getSharedPreferences(Global.PREFS,MODE_PRIVATE);  // FIXME
-		
-		mPullToRefreshAttacher = createPullToRefreshAttacher();
-	    mPullToRefreshAttacher.setRefreshableView(mEmergencyScrollView, this);
+		emergencyMsgLoader = (ImageView)findViewById(R.id.emergencyMsgLoader);
+		emergencyListLoader = (ImageView)findViewById(R.id.emergencyListLoader);
+		mEmergencyMsgTV = (WebView) findViewById(R.id.emergencyMsgTV);
+		contactsListView = (ListView) findViewById(R.id.emergencyContactListView);
+
+		pref = this.getSharedPreferences(Global.PREFS,MODE_PRIVATE);  
+
+		uiContactsHandler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				super.handleMessage(msg);
+				updateContacts();
+			}
+		};
+
+		uiStatusHandler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				super.handleMessage(msg);
+				updateEmergencyText();
+				C2DMReceiver.markNotificationAsRead(mContext, "emergencyinfo:");
+			}
+		};
+
+		msgPullToRefreshAttacher = createPullToRefreshAttacher();
+		contactsPullToRefreshAttacher = createPullToRefreshAttacher();
+
+	    msgPullToRefreshAttacher.setRefreshableView(mEmergencyMsgTV, this);
+	    contactsPullToRefreshAttacher.setRefreshableView(contactsListView, this);
 	    
-		getData();
+		msgPullToRefreshAttacher.setEnabled(false);
+	    contactsPullToRefreshAttacher.setEnabled(false);
+
+		emergencyMsgLoader.setVisibility(View.VISIBLE);
+		emergencyListLoader.setVisibility(View.VISIBLE);
+	    
+		EmergencyParser.fetchStatus(this, uiStatusHandler);
+		EmergencyParser.fetchContacts(this, uiContactsHandler);
+	}
+	
+	private void updateContacts() {	
+		emergencyListLoader.setVisibility(View.GONE);
+		
+		contactsListView.setVisibility(View.VISIBLE);
+		final EmergencyDB db = EmergencyDB.getInstance(this);
+		EmergencyContactsAdapter adapter = new EmergencyContactsAdapter(this, db.getLimitedContactsCursor());
+
+		contactsListView.setAdapter(adapter);
+
+		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		final TwoLineActionRow moreContactsRow = (TwoLineActionRow) inflater.inflate(R.layout.boring_action_row, null);;
+		moreContactsRow.setTitle("More Contacts");
+		contactsListView.addFooterView(moreContactsRow);
+
+		contactsListView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+									
+					if(view == moreContactsRow) {
+						Intent intent = new Intent(mContext, EmergencyContactsActivity.class);
+						startActivity(intent);
+						return;
+					}
+					
+					Contact c = db.getContact(position);
+					String numericPhone = PhoneNumberUtils.convertKeypadLettersToDigits(c.phone);
+					Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + numericPhone));
+					startActivity(intent);
+				}
+		});
 		
 	}
 
-	private static String noticeTemplate = "<html><body style=\"padding: 7px;\">%s\n<p>Posted %s</p></body></html>";
-	/****************************************************/
 	private void updateEmergencyText() {
 		emergencyItem = EmergencyParser.getStatus();
 
@@ -99,9 +151,7 @@ public class EmergencyActivity extends NewModuleActivity implements OnRefreshLis
 		setTitle(emergencyItem.title);
 		String html;
 		
-		mEmergencyMsgTV = (WebView) findViewById(R.id.emergencyMsgTV);
 		mEmergencyMsgTV.setVisibility(View.VISIBLE);
-		mEmergencyMsgLoader.setVisibility(View.GONE);
 		
 		if (emergencyItem.unixtime > 0) {
 			Date postDate = new Date(emergencyItem.unixtime * 1000);
@@ -113,103 +163,18 @@ public class EmergencyActivity extends NewModuleActivity implements OnRefreshLis
 		}
 		
 		mEmergencyMsgTV.loadDataWithBaseURL(null, html, "text/html", "utf-8", null);
-	}
-	
-	private void updateView() {
-		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+	    msgPullToRefreshAttacher.setEnabled(true);
+	    msgPullToRefreshAttacher.setRefreshing(false);
+	    contactsPullToRefreshAttacher.setEnabled(true);
+	    contactsPullToRefreshAttacher.setRefreshing(false);
 
-		mListView = (ListView) findViewById(R.id.emergencyListView);
-		mListView.setVisibility(View.VISIBLE);
-		mEmergencyListLoader.setVisibility(View.GONE);
-		//ImageView backgroundView = (ImageView) listHeader.findViewById(R.id.graySectionHeaderBackground);
-		//int height = backgroundView.getDrawable().getIntrinsicHeight();
-		//FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(LayoutParams.FILL_PARENT, height);
-		//FrameLayout wrapper = (FrameLayout) listHeader.findViewById(R.id.graySectionHeaderWrapper);
-		//wrapper.setLayoutParams(params);
-		final EmergencyDB db = EmergencyDB.getInstance(this);
-		
-		EmergencyContactsAdapter adapter = null;
-		adapter = new EmergencyContactsAdapter(this, db.getLimitedContactsCursor());
-
-		final TwoLineActionRow moreContactsRow = (TwoLineActionRow) inflater.inflate(R.layout.boring_action_row, null);;
-		moreContactsRow.setTitle("More Contacts");
-		mListView.addFooterView(moreContactsRow);
-		
-		mListView.setAdapter(adapter);
-		mListView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position,
-					long id) {
-				
-				if(view == moreContactsRow) {
-					Intent intent = new Intent(mContext, EmergencyContactsActivity.class);
-					startActivity(intent);
-					return;
-				}
-				
-				Contact c = db.getContact(position);
-				String numericPhone = PhoneNumberUtils.convertKeypadLettersToDigits(c.phone);
-				Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + numericPhone));
-				startActivity(intent);
-			}
-		});
-	}
-	
-	/****************************************************/
-	void getData() {
-		
-		// TODO combine into single handler/runnable
-		
-		// this Handler will run on this thread (UI)
-		final Handler uiHandler = new Handler() {
-			@Override
-			public void handleMessage(Message msg) {
-				updateView();
-			}
-		};
-		
-		// TODO: don't wait for both parts to load before showing stuff to the user
-		final Handler nextHandler = new Handler() {
-			@Override
-			public void handleMessage(Message msg) {
-				// mark emergency as read
-				updateEmergencyText();
-				C2DMReceiver.markNotificationAsRead(mContext, "emergencyinfo:");
-			}
-		};
-		
-		mEmergencyMsgLoader.setVisibility(View.VISIBLE);
-		mEmergencyListLoader.setVisibility(View.VISIBLE);
-		LoadingUIHelper.startLoadingImage(new Handler(), mEmergencyMsgLoader);
-		LoadingUIHelper.startLoadingImage(new Handler(), mEmergencyListLoader);
-		
-		EmergencyParser.fetchStatus(this, nextHandler);
-		EmergencyParser.fetchContacts(mContext, uiHandler);
-	}
-	
-	void refresh() {
-		
-		Handler handler = new Handler() {
-			@Override
-			public void handleMessage(Message msg) {
-				super.handleMessage(msg);
-				updateEmergencyText();
-				mPullToRefreshAttacher.setRefreshComplete();
-			}
-		};
-
-		EmergencyParser.refreshStatus(this, handler);
-	}
-	
-	// default implementation for primary, and secondary menu items.
-	@Override
-	protected List<MITMenuItem> getPrimaryMenuItems() {
-		return null;
+	    emergencyMsgLoader.setVisibility(View.GONE);
 	}
 	
 	@Override
 	public boolean isModuleHomeActivity() {
-		return true;
+		return false;
 	}
 
 	@Override
@@ -221,14 +186,16 @@ public class EmergencyActivity extends NewModuleActivity implements OnRefreshLis
 	@Override
 	protected boolean isScrollable() {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
-	protected void onOptionSelected(String optionId) { }
-
+	protected void onOptionSelected(String optionId) {
+		// TODO Auto-generated method stub
+	}
+	
 	@Override
 	public void onRefreshStarted(View view) {
-		refresh();		
+		EmergencyParser.fetchStatus(this, uiStatusHandler);
 	}
 }
