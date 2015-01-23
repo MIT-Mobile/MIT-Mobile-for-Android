5a0f22aa24137c5f664dda719316742736164cac
Nelson Chen <njchen@mit.edu>
2015-01-23 10:25:28 -0800

    abstrct mapitem

--- app/src/main/java/edu/mit/mitmobile2/maps/MapItem.java
+++ app/src/main/java/edu/mit/mitmobile2/maps/MapItem.java
@@ -1,6 +1,16 @@
 package edu.mit.mitmobile2.maps;
+
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.Polygon;
 
 public abstract class MapItem {
+
+    private final int markType = 1;
+    private final int polylineType = 2;
+    private final int polygonType = 3;
+
     public abstract Marker getMarker();
+    public abstract Polyline getPolyline();
+    public abstract Polygon getPolygon();
 }
