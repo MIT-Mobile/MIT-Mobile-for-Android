0a8b43ab3bf8de59f68935727f1617b491c694e9
Nelson Chen <njchen@mit.edu>
2015-01-29 20:48:15 -0800

    remove some code I played with in MapsActivity.java

--- app/src/main/java/edu/mit/mitmobile2/maps/MapsActivity.java
+++ app/src/main/java/edu/mit/mitmobile2/maps/MapsActivity.java
@@ -30,10 +30,9 @@ public class  MapsActivity extends MITModuleActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_maps);
-        //int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
 
         mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        //mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         //MAP_TYPE_NONE   		No base map tiles.
         //MAP_TYPE_NORMAL		Basic maps.
@@ -42,18 +41,10 @@ public class  MapsActivity extends MITModuleActivity {
         //MAP_TYPE_TERRAIN		Terrain maps.
         //final LatLng CIU = new LatLng(35.21843892856462, 33.41662287712097);
 
-        final LatLng CIU1 = new LatLng(42.359858, -71.09913);
-        final LatLng CIU2 = new LatLng(42.354934, -71.104633);
-        ArrayList<LatLng> latLngList = new ArrayList<LatLng>();
-        latLngList.add(CIU1);
-        latLngList.add(CIU2);
-        Marker ciu1 = mMap.addMarker(new MarkerOptions().position(CIU1).title("MIT Headquater"));
-        Marker ciu2 = mMap.addMarker(new MarkerOptions().position(CIU2).title("IST"));
-        ciu1.showInfoWindow();
-        ciu2.showInfoWindow();
-        ciu1.setDraggable(true);
-        ciu2.setDraggable(true);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(CIU1, 14));
+        //set initial latlng for zoom in MIT area
+        final LatLng initialLatLng = new LatLng(42.359858, -71.09913);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(initialLatLng, 14));
 
         Intent intent = getIntent();
         if(intent.hasExtra(MapsActivity.MAP_ITEMS)) {
@@ -62,22 +53,14 @@ public class  MapsActivity extends MITModuleActivity {
                 mMap.addMarker(this.mapItems.get(i).getMarkerOptions());
             }
         }
-		/*
-		Marker ciu = mMap.addMarker(new MarkerOptions().position(CIU1).title("My Office"));
-		ciu.setDraggable(true);
-		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(CIU1, 14));
-		*/
-        //zoomToCoverAllMarkers(latLngList, mMap);
+
     }
 
 
     private static void zoomToCoverAllMarkers(ArrayList<LatLng> latLngList, GoogleMap googleMap)
     {
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        /*for (Marker marker : markers)
-        {
-            builder.include(marker.getPosition());
-        }*/
+
         for (LatLng marker : latLngList)
         {
             builder.include(marker);
