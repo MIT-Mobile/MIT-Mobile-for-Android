46c36bb85c341ca3d876400300e4a201e140c117
Chris Kudlack <chris@intrepid.io>
2015-03-05 09:56:16 -0500

    Removed commented code, put in Timber call

--- app/src/main/java/edu/mit/mitmobile2/shuttles/ShuttlesActivity.java
+++ app/src/main/java/edu/mit/mitmobile2/shuttles/ShuttlesActivity.java
@@ -7,9 +7,9 @@ import edu.mit.mitmobile2.shuttles.model.MITShuttlePredictionWrapper;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+import timber.log.Timber;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -35,68 +35,14 @@ public class ShuttlesActivity extends MITModuleActivity {
         apiClient.get(Constants.SHUTTLES, Constants.Shuttles.PREDICTIONS_PATH, null, queries, new Callback<List<MITShuttlePredictionWrapper.Predictions>>() {
             @Override
             public void success(List<MITShuttlePredictionWrapper.Predictions> mitShuttlePredictionWrapper, Response response) {
-                Log.d("ZZZ", "onResume");
                 Toast.makeText(ShuttlesActivity.this, "Finished", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void failure(RetrofitError error) {
-                Log.d("ZZZ", "onResume");
+                Timber.e(error, TAG);
             }
         });
-
-        /*HashMap<String, String> pathParams = new HashMap<>();
-        pathParams.put("route", "tech");
-        pathParams.put("stop", "kendsq_d");
-
-        apiClient.get(Constants.SHUTTLES, "/shuttles/routes/{route}/stops/{stop}", pathParams, null, new Callback<MITShuttleStopWrapper>() {
-            @Override
-            public void success(MITShuttleStopWrapper mitShuttleStopWrapper, Response response) {
-                Log.d("ZZZ", "onResume");
-                Log.d("ZZZ", "onResume");
-
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d("ZZZ", "onResume");
-                Log.d("ZZZ", "onResume");
-
-            }
-        });*/
-
-        /*apiClient.get(Constants.SHUTTLES, "/shuttles/routes", null, null, new Callback<List<MITShuttleRouteWrapper>>() {
-            @Override
-            public void success(List<MITShuttleRouteWrapper> mitShuttleRouteWrappers, Response response) {
-                Log.d("ZZZ", "onResume");
-                Toast.makeText(ShuttlesActivity.this, "Finished", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d("ZZZ", "onResume");
-                Log.d("ZZZ", "onResume");
-            }
-        });*/
-
-        /*HashMap<String, String> pathParams = new HashMap<>();
-        pathParams.put("route", "tech");
-
-        apiClient.get(Constants.SHUTTLES, "/shuttles/routes/{route}", pathParams, null, new Callback<MITShuttleRouteWrapper>() {
-            @Override
-            public void success(MITShuttleRouteWrapper mitShuttleRouteWrapper, Response response) {
-                Log.d("ZZZ", "onResume");
-                Log.d("ZZZ", "onResume");
-
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d("ZZZ", "onResume");
-                Log.d("ZZZ", "onResume");
-
-            }
-        });*/
 
 
     }
