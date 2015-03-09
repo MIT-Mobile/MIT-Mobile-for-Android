252cf55d1096b34955208ecddb568d9ed0e14e53
shannon huang <xiaochenhuang1989@gmail.com>
2015-03-09 13:34:21 -0400

    Squashed commit of the following: setup mitShuttleRoutes adapter

--- app/src/main/java/edu/mit/mitmobile2/MITActivity.java
+++ app/src/main/java/edu/mit/mitmobile2/MITActivity.java
@@ -1,17 +1,30 @@
 package edu.mit.mitmobile2;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
-/**
- * Created by sseligma on 2/24/15.
- */
-public class MITActivity extends ActionBarActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+
+public class MITActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static final long UPDATE_INTERVAL = 1000;
 
     protected String TAG;
     protected Context mContext;
     protected MITAPIClient apiClient;
+    protected GoogleApiClient googleApiClient;
+    protected LocationRequest locationRequest;
+    protected Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +32,50 @@ public class MITActivity extends ActionBarActivity {
         TAG = this.getClass().getSimpleName();
         mContext = this;
         this.apiClient = new MITAPIClient(mContext);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                googleApiClient, locationRequest, this
+        );
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection has ben suspend");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "GoogleApiClient connection has failed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.location = location;
     }
 }
