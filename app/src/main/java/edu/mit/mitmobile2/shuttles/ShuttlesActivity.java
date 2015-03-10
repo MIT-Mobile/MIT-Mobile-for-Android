3a6a1976190341eb29f399105931c3573961b97b
shannon huang <xiaochenhuang1989@gmail.com>
2015-03-10 10:32:59 -0400

    avoid crashing when getPredictions is null

--- app/src/main/java/edu/mit/mitmobile2/shuttles/ShuttlesActivity.java
+++ app/src/main/java/edu/mit/mitmobile2/shuttles/ShuttlesActivity.java
@@ -159,13 +159,15 @@ public class ShuttlesActivity extends MITModuleActivity {
                 stopParams, null, new Callback<MITShuttleStopWrapper>() {
                     @Override
                     public void success(MITShuttleStopWrapper mitShuttleStopWrapper, Response response) {
-                        if (mitShuttleStopWrapper.getPredictions().size() != 0) {
-                            if (isFirstStop) {
-                                mitShuttle.setFirstMinute(mitShuttleStopWrapper.getPredictions().get(0).
-                                        getSeconds() / 60 + "m");
-                            } else {
-                                mitShuttle.setSecondMinute(mitShuttleStopWrapper.getPredictions().get(0).
-                                        getSeconds() / 60 + "m");
+                        if (mitShuttleStopWrapper.getPredictions() != null) {
+                            if (mitShuttleStopWrapper.getPredictions().size() != 0) {
+                                if (isFirstStop) {
+                                    mitShuttle.setFirstMinute(mitShuttleStopWrapper.getPredictions().get(0).
+                                            getSeconds() / 60 + "m");
+                                } else {
+                                    mitShuttle.setSecondMinute(mitShuttleStopWrapper.getPredictions().get(0).
+                                            getSeconds() / 60 + "m");
+                                }
                             }
                         }
                         mitShuttleAdapter.notifyDataSetChanged();
