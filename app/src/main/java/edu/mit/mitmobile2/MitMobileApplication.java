9df6485ee6efe8e7fe0c88b9e4a9aa1adc4fbceb
Chris Kudlack <chris@intrepid.io>
2015-03-17 14:27:00 -0400

    Added global retrofit error listener

--- app/src/main/java/edu/mit/mitmobile2/MitMobileApplication.java
+++ app/src/main/java/edu/mit/mitmobile2/MitMobileApplication.java
@@ -2,6 +2,11 @@ package edu.mit.mitmobile2;
 
 import android.accounts.Account;
 import android.app.Application;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
 
 import timber.log.Timber;
 
@@ -21,14 +26,45 @@ public class MitMobileApplication extends Application {
 
     public static DBAdapter dbAdapter;
 
+    public static Bus bus;
+
+    GlobalOttoListener listener;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         dbAdapter = new DBAdapter(this);
+        bus = new Bus();
+        listener = new GlobalOttoListener();
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+    private class GlobalOttoListener {
+        public GlobalOttoListener() {
+            bus.register(this);
+        }
+
+        @Subscribe
+        public void onRetrofitError(OttoBusEvent.RetrofitFailureEvent event) {
+            Timber.e("GLOBAL RETROFIT ERROR");
+            String message = event.getError().getMessage();
+            if (event.getError().isNetworkError()) {
+                Toast.makeText(MitMobileApplication.this, "Network error, please check connection", Toast.LENGTH_SHORT).show();
+            } else if (event.getError().getResponse() != null) {
+                int status = event.getError().getResponse().getStatus();
+                if (status == 500) {
+                    Toast.makeText(MitMobileApplication.this, "Internal Server Error", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MitMobileApplication.this, "HTTP " + status + " Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+            if (!TextUtils.isEmpty(message)) {
+                Timber.e(message);
+            }
         }
     }
 }
