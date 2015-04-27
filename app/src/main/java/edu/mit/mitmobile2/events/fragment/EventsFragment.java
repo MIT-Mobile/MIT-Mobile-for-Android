a84eb1005455dab6c5a74dcb1348b728d7f34c31
Glenn R. Martin <grmartin@mac.com>
2015-04-27 11:53:39 -0400

    Killing most test code left over, leaving a small show for Alex in comments so he knows how to invoke it.

--- app/src/main/java/edu/mit/mitmobile2/events/fragment/EventsFragment.java
+++ app/src/main/java/edu/mit/mitmobile2/events/fragment/EventsFragment.java
@@ -1,7 +1,5 @@
 package edu.mit.mitmobile2.events.fragment;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
@@ -11,12 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.mit.mitmobile2.R;
-import edu.mit.mitmobile2.events.EventManager;
-import edu.mit.mitmobile2.events.model.MITCalendar;
-import edu.mit.mitmobile2.shared.logging.LoggingManager;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 public class EventsFragment extends Fragment {
 public static final String TAG = "EventsFragment";
@@ -36,40 +28,17 @@ public static final String TAG = "EventsFragment";
     public void onStart() {
         super.onStart();
 
-        EventManager.getCalendars(getActivity(), new Callback<List<MITCalendar>>() {
-            @Override
-            public void success(List<MITCalendar> mitCalendars, Response response) {
-                LoggingManager.Log.d(TAG, "MITCalendar => "+mitCalendars+ " => "+response);
-                LoggingManager.Log.d(TAG, "MITCalendar => "+mitCalendars+ " => "+response);
-
-                gotCalendars(mitCalendars);
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                LoggingManager.Log.d(TAG, "ERROR => "+error);
-            }
-        });
-    }
-
-    private void gotCalendars(List<MITCalendar> mitCalendars) {
-
-        MITCalendar lastOne = mitCalendars.get(mitCalendars.size() - 1);
-
-        EventManager.getCalendarDetail(getActivity(), lastOne, new Callback<MITCalendar>() {
-            @Override
-            public void success(MITCalendar mitCalendar, Response response) {
-                LoggingManager.Log.d(TAG, "MITCalendar => "+mitCalendar+ " => "+response);
-                LoggingManager.Log.d(TAG, "MITCalendar => "+mitCalendar+ " => "+response);
-
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                LoggingManager.Log.d(TAG, "ERROR => "+error);
-
-            }
-        });
+//        EventManager.ServiceCall call = EventManager.getCalendars(getActivity(), new Callback<List<MITCalendar>>() {
+//            @Override
+//            public void success(List<MITCalendar> mitCalendars, Response response) {
+//                LoggingManager.Log.d(TAG, "MITCalendar => "+mitCalendars+ " => "+response);
+//            }
+//
+//            @Override
+//            public void failure(RetrofitError error) {
+//                LoggingManager.Log.d(TAG, "ERROR => "+error);
+//            }
+//        });
     }
 
     @Override
