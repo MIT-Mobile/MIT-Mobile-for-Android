28e988b6acbdb190176d9425e2e943733c4f651e
PhilIntrepid <pdc52@cornell.edu>
2015-05-12 11:11:43 -0400

    Initial filters activity UI

---
+++ app/src/main/java/edu/mit/mitmobile2/dining/FiltersActivity.java
@@ -0,0 +1,18 @@
+package edu.mit.mitmobile2.dining;
+
+import android.os.Bundle;
+
+import edu.mit.mitmobile2.MITActivity;
+import edu.mit.mitmobile2.R;
+
+/**
+ * Created by philipcorriveau on 5/12/15.
+ */
+public class FiltersActivity extends MITActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+    }
+}
