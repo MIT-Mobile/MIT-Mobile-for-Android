205602d30daa3a6890fbd18dc8200f54a2f77b67
Alex <kotsubaaa@gmail.com>
2015-05-13 17:53:12 +0300

    added retail adapter

--- app/src/main/java/edu/mit/mitmobile2/dining/fragments/HouseDiningFragment.java
+++ app/src/main/java/edu/mit/mitmobile2/dining/fragments/HouseDiningFragment.java
@@ -6,17 +6,20 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import edu.mit.mitmobile2.R;
 import edu.mit.mitmobile2.dining.adapters.HouseDiningAdapter;
 import edu.mit.mitmobile2.dining.interfaces.Updateable;
 import edu.mit.mitmobile2.dining.model.MITDiningDining;
+import edu.mit.mitmobile2.dining.model.MITDiningHouseVenue;
+import edu.mit.mitmobile2.dining.model.MITDiningLinks;
 import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 /**
  * Created by serg on 5/8/15.
  */
-public class HouseDiningFragment extends Fragment implements Updateable {
+public class HouseDiningFragment extends Fragment implements Updateable, AdapterView.OnItemClickListener {
 
     private StickyListHeadersListView listView;
 
@@ -33,8 +36,10 @@ public class HouseDiningFragment extends Fragment implements Updateable {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dining_house, null);
 
-        listView = (StickyListHeadersListView) view.findViewById(R.id.list_dining_house);
         adapter = new HouseDiningAdapter(getActivity(), null);
+
+        listView = (StickyListHeadersListView) view.findViewById(R.id.list_dining_house);
+        listView.setOnItemClickListener(this);
         listView.setAdapter(adapter);
 
         return view;
@@ -48,4 +53,21 @@ public class HouseDiningFragment extends Fragment implements Updateable {
         adapter.setMitDiningDining(mitDiningDining);
     }
 
+    /* AdapterView.OnItemClickListener */
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Object selectedItem = adapter.getItem(position);
+
+        if (selectedItem instanceof MITDiningDining) {                              // announcement
+            MITDiningDining dining = (MITDiningDining) selectedItem;
+            // TODO: add logic here
+        } else if (selectedItem instanceof MITDiningHouseVenue) {                   // venue
+            MITDiningHouseVenue houseVenue = (MITDiningHouseVenue) selectedItem;
+            // TODO: add logic here
+        } else if (selectedItem instanceof MITDiningLinks) {                        // resource
+            MITDiningLinks link = (MITDiningLinks) selectedItem;
+            // TODO: add logic here
+        }
+    }
 }
--- app/src/main/java/edu/mit/mitmobile2/dining/fragments/RetailFragment.java
+++ app/src/main/java/edu/mit/mitmobile2/dining/fragments/RetailFragment.java
@@ -6,15 +6,24 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import edu.mit.mitmobile2.R;
+import edu.mit.mitmobile2.dining.adapters.RetailAdapter;
 import edu.mit.mitmobile2.dining.interfaces.Updateable;
 import edu.mit.mitmobile2.dining.model.MITDiningDining;
+import edu.mit.mitmobile2.dining.model.MITDiningRetailVenue;
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 /**
  * Created by serg on 5/8/15.
  */
-public class RetailFragment extends Fragment implements Updateable {
+public class RetailFragment extends Fragment implements Updateable, AdapterView.OnItemClickListener {
+
+    private StickyListHeadersListView listView;
+
+    private RetailAdapter adapter;
+    private MITDiningDining mitDiningDining;
 
     public static RetailFragment newInstance() {
         RetailFragment fragment = new RetailFragment();
@@ -26,6 +35,11 @@ public class RetailFragment extends Fragment implements Updateable {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dining_retail, null);
 
+        adapter = new RetailAdapter(getActivity());
+
+        listView = (StickyListHeadersListView) view.findViewById(R.id.list_retail);
+        listView.setAdapter(adapter);
+
         return view;
     }
 
@@ -33,6 +47,15 @@ public class RetailFragment extends Fragment implements Updateable {
 
     @Override
     public void onDining(MITDiningDining mitDiningDining) {
+        this.mitDiningDining = mitDiningDining;
+        adapter.setRetailVenues(mitDiningDining.getVenues().getRetail());
+    }
+
+    /* AdapterView.OnItemClickListener */
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        MITDiningRetailVenue selectedVenue = adapter.getItem(position);
         // TODO: add logic here
     }
 }
