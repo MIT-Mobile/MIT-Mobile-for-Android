8d761e407a692634aa82ec72eff18713ff2c13ea
Alex <kotsubaaa@gmail.com>
2015-06-09 14:16:39 +0300

    credits screen added

--- app/src/main/java/edu/mit/mitmobile2/about/activities/CreditsActivity.java
+++ app/src/main/java/edu/mit/mitmobile2/about/activities/CreditsActivity.java
@@ -3,6 +3,7 @@ package edu.mit.mitmobile2.about.activities;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 import edu.mit.mitmobile2.R;
 
@@ -11,9 +12,17 @@ import edu.mit.mitmobile2.R;
  */
 public class CreditsActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        webView = (WebView) findViewById(R.id.webview);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        // TODO: add real names to "credits_current.html" on project release
+        webView.loadUrl("file:///android_asset/credits_current.html");
     }
 }
