7e6589b66554a69563d46667233d844d5b64049f
Alex <kotsubaaa@gmail.com>
2015-06-15 16:05:53 +0300

    scanner screen is implemented

--- app/src/main/java/edu/mit/mitmobile2/qrreader/QrReaderFragment.java
+++ app/src/main/java/edu/mit/mitmobile2/qrreader/QrReaderFragment.java
@@ -3,21 +3,27 @@ package edu.mit.mitmobile2.qrreader;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.graphics.PointF;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 
 import edu.mit.mitmobile2.R;
 
-public class QrReaderFragment extends Fragment implements QRCodeReaderView.OnQRCodeReadListener {
+public class QrReaderFragment extends Fragment implements QRCodeReaderView.OnQRCodeReadListener, View.OnClickListener {
 
+    private TextView textViewDisclaimer;
+    private TextView textViewInfo;
+    private TextView textViewAdvanced;
     private QRCodeReaderView qrCodeReaderView;
 
     private int savedOrientation;
@@ -34,16 +40,70 @@ public class QrReaderFragment extends Fragment implements QRCodeReaderView.OnQRC
         activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.content_qrreader, null);
 
+        textViewDisclaimer = (TextView) view.findViewById(R.id.scanner_tv_disclaimer);
+        textViewInfo = (TextView) view.findViewById(R.id.scanner_tv_info);
+        textViewAdvanced = (TextView) view.findViewById(R.id.scanner_tv_advanced);
         qrCodeReaderView = (QRCodeReaderView) view.findViewById(R.id.qrdecoderview);
+
         qrCodeReaderView.setOnQRCodeReadListener(this);
+        textViewInfo.setOnClickListener(this);
+        textViewAdvanced.setOnClickListener(this);
 
         return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_scanner, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_scanner_history: {
+                // TODO: navigate to history screen
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDetach() {
+        getActivity().setRequestedOrientation(savedOrientation);
+        super.onDetach();
+    }
+
+    /* View.OnClickListener */
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.scanner_tv_info: {
+                // TODO: navigate to "info" screen
+            }
+            break;
+            case R.id.scanner_tv_advanced: {
+                // TODO: navigate to "advanced" screen
+            }
+            break;
+        }
+    }
+
+    /* QRCodeReaderView.OnQRCodeReadListener */
 
     @Override
     public void onQRCodeRead(String s, PointF[] pointFs) {
@@ -57,14 +117,8 @@ public class QrReaderFragment extends Fragment implements QRCodeReaderView.OnQRC
     }
 
     @Override
-    public void onDetach() {
-        getActivity().setRequestedOrientation(savedOrientation);
-        super.onDetach();
-    }
-
-    @Override
     public void cameraNotFound() {
-
+        textViewDisclaimer.setText(R.string.scan_no_camera);
     }
 
     @Override
